182d151eb86010b7d86495f21b48296933f88ef7
Mauro <mauro.erta@gmail.com>
2018-04-28 18:31:44 +0200

    CommandServiceProvider

---
+++ src/Commands/CommandServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Mauro\Commands;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommandServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Traits::class
+            ]);
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}

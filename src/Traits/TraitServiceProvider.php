50fa832f146620454faed2fcda8e5560b49ec46d
Mauro <mauro.erta@gmail.com>
2018-05-01 20:40:05 +0200

    Publish migrations instead of load in the service provider.

--- src/Traits/TraitServiceProvider.php
+++ src/Traits/TraitServiceProvider.php
@@ -16,6 +16,11 @@ class TraitServiceProvider extends ServiceProvider
             __DIR__.'/config/me_trait.php' => config_path('me_trait.php'),
         ]);
 
+        $this->publishes([
+            __DIR__.'/migrations/draftable_alter_tables.php' => database_path('migrations/'.date('Y_m_d_His').'_draftable_alter_tables.php'),
+            __DIR__.'/migrations/slugable_alter_tables.php' => database_path('migrations/'.date('Y_m_d_His').'_slugable_alter_tables.php'),
+        ]);
+
         $this->loadMigrationsFrom(__DIR__.'/migrations');
     }
 

27ba3d22410172080947b089414cb9be9be168a1
Mauro <mauro.erta@gmail.com>
2018-05-02 17:17:48 +0200

    Added 2 middlewares, one for check if the authenticated user is an admin and on to force the https in production

---
+++ src/Http/Middleware/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ME\Http\Middleware;
+
+use Auth;
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next) {
+        if(Auth::guest())
+            abort(403);
+
+        if(Auth::user()->isAdmin())
+            abort(403);
+
+        return $next($request);
+    }
+}
---
+++ src/Http/Middleware/ForceHttps.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ME\Http\Middleware;
+
+use Closure;
+
+class ForceHttps
+{
+    /**
+     * Handle the request
+     * @param  \Illuminate\Http\Request $request
+     * @param  Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next) {
+        if (!$request->secure() && env('APP_ENV') === 'production') {
+            $request->setTrustedProxies([$request->getClientIp()]);
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}

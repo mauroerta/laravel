a7d2e670944e1620058a0e665f8d61f8d98d0d0e
Mauro <mauro.erta@gmail.com>
2018-05-02 10:44:51 +0200

    Static name for migrations

--- src/Traits/TraitServiceProvider.php
+++ src/Traits/TraitServiceProvider.php
@@ -17,9 +17,8 @@ class TraitServiceProvider extends ServiceProvider
         ]);
 
         $this->publishes([
-            __DIR__.'/migrations/draftable_alter_tables.php' => database_path('migrations/'.date('Y_m_d_His').'_draftable_alter_tables.php'),
-            __DIR__.'/migrations/slugable_alter_tables.php' => database_path('migrations/'.date('Y_m_d_His').'_slugable_alter_tables.php'),
-        ]);
+            __DIR__.'/migrations' => database_path('migrations'),
+        ], 'database');
     }
 
     /**

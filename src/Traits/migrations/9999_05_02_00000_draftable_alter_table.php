26624cfeb6236e0aa543b0323a7fbb01e07302d0
Mauro <mauro.erta@gmail.com>
2019-03-07 23:57:35 +0100

    fix

--- src/Traits/migrations/9999_05_02_00000_draftable_alter_table.php
+++ src/Traits/migrations/9999_05_02_00000_draftable_alter_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DraftableAlterTables extends Migration
+class DraftableAlterTable extends Migration
 {
   /**
    * Run the migrations.
--- src/Traits/migrations/9999_05_02_00001_slugable_alter_table.php
+++ src/Traits/migrations/9999_05_02_00001_slugable_alter_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SlugableAlterTables extends Migration
+class SlugableAlterTable extends Migration
 {
   /**
    * Run the migrations.
